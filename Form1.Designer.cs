29739202765a9e350d6cc9bc2d9d580cb452096b
Oscar G. G <o_gom_gar@hotmail.com>
2010-09-28 22:47:26 +0200

    First release of the project

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace PersonalClock
+{
+    partial class frmPersonalClock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPersonalClock));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.numMinutes = new System.Windows.Forms.NumericUpDown();
+            this.lblMinutes = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.temporizador = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numMinutes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Tag = "";
+            this.notifyIcon1.Text = "Personal alarm";
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // numMinutes
+            // 
+            this.numMinutes.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numMinutes.Location = new System.Drawing.Point(160, 78);
+            this.numMinutes.Name = "numMinutes";
+            this.numMinutes.Size = new System.Drawing.Size(124, 44);
+            this.numMinutes.TabIndex = 0;
+            // 
+            // lblMinutes
+            // 
+            this.lblMinutes.AutoSize = true;
+            this.lblMinutes.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMinutes.Location = new System.Drawing.Point(36, 83);
+            this.lblMinutes.Name = "lblMinutes";
+            this.lblMinutes.Size = new System.Drawing.Size(116, 31);
+            this.lblMinutes.TabIndex = 1;
+            this.lblMinutes.Text = "Minutes";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(12, 214);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(272, 47);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start countdown!";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // temporizador
+            // 
+            this.temporizador.Interval = 60000;
+            this.temporizador.Tick += new System.EventHandler(this.temporizador_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(238, 286);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "By OGG";
+            // 
+            // frmPersonalClock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(296, 308);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblMinutes);
+            this.Controls.Add(this.numMinutes);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmPersonalClock";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Personal alarm";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.numMinutes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.NumericUpDown numMinutes;
+        private System.Windows.Forms.Label lblMinutes;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer temporizador;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PersonalClock
+{
+    public partial class frmPersonalClock : Form
+    {
+        public frmPersonalClock()
+        {
+            InitializeComponent();
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon1.Visible = false;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                minimizar();
+            }
+        }
+        private void minimizar()
+        {
+            this.WindowState = FormWindowState.Minimized;
+            this.Visible = false;
+            notifyIcon1.Visible = true;
+        }
+        private void maximizar()
+        {
+            this.Visible = true;
+            notifyIcon1.Visible = false;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (numMinutes.Value == 0) return;
+            temporizador.Start();
+            numMinutes.Enabled = false;
+            btnStart.Enabled = false;
+            btnStart.Text = "Waiting...";
+            minimizar();
+        }
+
+        private void temporizador_Tick(object sender, EventArgs e)
+        {
+            if (numMinutes.Value == 0)
+            {
+                btnStart.Enabled = true;
+                btnStart.Text = "Start countdown!";
+                temporizador.Stop();
+                maximizar();
+                this.Show();
+                this.Activate();
+                numMinutes.Enabled = true;
+                MessageBox.Show("Countdown has finished!", "Time out", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            numMinutes.Value = numMinutes.Value - 1;
+            notifyIcon1.Text = numMinutes.Value + " minutes remaining";
+        }
+    }
+}
